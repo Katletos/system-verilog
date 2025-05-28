c53957c5bef9d7743c3fddd93d2d8c7545183329
Mikita <40485432+Katletos@users.noreply.github.com>
2025-05-28 10:53:16 +0300

    Create fsm_2_1.sv

---
+++ fsm_warmup/fsm_2_1.sv
@@ -0,0 +1,27 @@
+module fsm_2_1(
+    output logic z,
+    input logic clk, rst_n, x
+);
+
+typedef enum {
+    INVERT,
+    COPY
+} fsm_state;
+fsm_state state;
+
+
+always_ff @(posedge clk, negedge rst_n)
+    if (rst_n) begin 
+        state <= COPY;
+        z <= 0;
+    end else begin 
+        unique case (state)
+            COPY   : begin
+                state <= x ? INVERT : COPY;
+                z <= x;
+            end
+            INVERT : z <= ~x;
+        endcase
+    end
+
+endmodule
